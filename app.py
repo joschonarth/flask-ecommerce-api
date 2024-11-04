298d033813736210fcbb89ad7ef846be04ab5f71
joschonarth <joschonarth@gmail.com>
2024-11-04 15:58:37 -0300

    Add /api/products/add endpoint for add product

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -12,9 +12,15 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
 
-@app.route("/test")
-def hello_world():
-    return "Hello World"
+@app.route('/api/products/add', methods=['POST'])
+def add_product():
+    data = request.json
+    if 'name' in data and 'price' in data:
+        product = Product(name=data["name"], price=data["price"], description=data.get("description", ""))
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({"message": "Product added successfully"}), 201
+    return jsonify({"message": "Invalid product data"}), 400
 
 if __name__ == "__main__":
     app.run(debug=True)
