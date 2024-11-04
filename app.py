dadca0847d659ea48cde410c0721edf1e9ffad40
joschonarth <joschonarth@gmail.com>
2024-11-04 16:11:05 -0300

    Add DELETE route for remove product

--- app.py
+++ app.py
@@ -22,5 +22,14 @@ def add_product():
         return jsonify({"message": "Product added successfully"}), 201
     return jsonify({"message": "Invalid product data"}), 400
 
+@app.route('/api/products/delete/<int:product_id>', methods=['DELETE'])
+def delete_product(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        db.session.delete(product)
+        db.session.commit()
+        return jsonify({"message": "Product deleted successfully"}), 201
+    return jsonify({"message": "Product not found"}), 404
+
 if __name__ == "__main__":
     app.run(debug=True)
