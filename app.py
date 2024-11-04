16e0bad158484fdcdf81345daaa41c7f3729af6f
joschonarth <joschonarth@gmail.com>
2024-11-04 15:23:12 -0300

    Create Database

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
 
 @app.route("/test")
 def hello_world():
