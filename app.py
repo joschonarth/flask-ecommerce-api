318b21be6cc5ec0a4798abf3cfecf61b9af09a0a
joschonarth <joschonarth@gmail.com>
2024-11-04 22:54:02 -0300

    Create route to make checkout and clear the cart

--- app.py
+++ app.py
@@ -158,5 +158,15 @@ def view_cart():
         })
     return jsonify(cart_content)
 
+@app.route('/api/cart/checkout', methods=['POST'])
+@login_required
+def checkout():
+    user = User.query.get(int(current_user.id))
+    cart_items = user.cart
+    for item in cart_items:
+        db.session.delete(item)
+    db.session.commit()
+    return jsonify({"message": "Checkout successful. Cart has been cleared."})
+
 if __name__ == "__main__":
     app.run(debug=True)
